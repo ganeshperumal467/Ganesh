1389ad1e2caaf80d19a44514fa090d896430936e
ganeshperumal467 <52821597+ganeshperumal467@users.noreply.github.com>
2019-07-19 14:21:34 +0530

    Create ddd.py

---
+++ ddd.py
@@ -0,0 +1,7 @@
+alp=input()
+if(alp>='a'and alp<='z') or (alp>='A' and alp<='Z'):
+  if(alp=='A' or alp=='a' or alp=='E' or alp=='e' or alp=='I'
+    or alp=='i' or alp=='O' or alp=='o' or alp=='U' or alp=='u'):
+     print("Vowel")
+else:
+     print("Consonant")
